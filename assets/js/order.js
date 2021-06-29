fe92782f78ff17d71326097608169187f5476a1d
Steven Oyer <contact.stevenoyer@gmail.com>
2021-06-29 15:33:58 +0200

    Page de confirmation finale

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,6 +1,17 @@
 // Main function and auto called on load page
 (async function() {
     
-    
+    // Get params in URL
+    const params = new URLSearchParams(window.location.search);
+
+    // If order and params not empty 
+    if (params.get('order') != "" && params != "") {
+        // Get params order and print in element
+        document.getElementById('orderCommand').textContent = params.get('order')
+    }else {
+        // Return home page if empty params and order
+        location.assign('index.html')
+    }
+
 
 }());
